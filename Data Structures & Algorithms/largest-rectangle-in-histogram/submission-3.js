fa5908e7740e957420ee3bf90c23c27009416487
Aneerudh Prabhakaran <aneedroid13@gmail.com>
2026-07-07 07:08:32 +0530

    Add: largest-rectangle-in-histogram - submission-3

---
+++ Data Structures & Algorithms/largest-rectangle-in-histogram/submission-3.js
@@ -0,0 +1,28 @@
+class Solution {
+    /**
+     * @param {number[]} heights
+     * @return {number}
+     */
+    largestRectangleArea(heights) {
+        const stack = []; // Obj - [val, idx]
+        let max = 0;
+
+        for(let i = 0; i < heights.length; i++) {
+            let s = i;
+            while(stack.length && stack[stack.length - 1][0] > heights[i]) {
+                const [val, idx] = stack.pop();
+                // Calculate max area;
+                max = Math.max(max, (val * (i - idx)));
+                // Shift the start to previous index
+                s = idx;
+            }
+            stack.push([heights[i], s]);
+        }
+
+        while(stack.length) {
+            const [val, idx] = stack.pop();
+            max = Math.max(max, val * (heights.length - idx));
+        }
+        return max;
+    }
+}
