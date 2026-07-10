6542213deddf15882b9da816f54a6f8ee38f106c
Aneerudh Prabhakaran <aneedroid13@gmail.com>
2026-07-10 08:51:45 +0530

    Add: longest-substring-without-duplicates - submission-5

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-5.js
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {number}
+     */
+    lengthOfLongestSubstring(s) {
+        const set = new Set();
+        let l = 0;
+        let r = 0;
+        let max = 0;
+        while(r < s.length) {
+            while(set.has(s[r])) {
+                set.delete(s[l]);
+                l++;
+            }
+            set.add(s[r])
+            max = Math.max(max, r - l + 1);
+            r++;
+        }
+        return max;
+    }
+}
